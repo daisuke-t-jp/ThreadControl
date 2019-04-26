ad672e61869dc0487761a341b783647ba20e3d6f
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-04-26 14:30:13 +0900

    Change Tab to Whitespace(U+0020)

--- ThreadControl/ViewController.swift
+++ ThreadControl/ViewController.swift
@@ -10,22 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
-		let threadControl = ThreadControl.init(target: self, selector: #selector(ViewController.threadFunc(_:)), object: "Hello World", loopTimeInterval: 1)
-		threadControl.rawThread?.name = "Test-Thread"
-		threadControl.rawThread?.start()
-		
-		DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-			threadControl.rawThread?.cancel()
-		}
-	}
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let threadControl = ThreadControl.init(target: self, selector: #selector(ViewController.threadFunc(_:)), object: "Hello World", loopTimeInterval: 1)
+    threadControl.rawThread?.name = "Test-Thread"
+    threadControl.rawThread?.start()
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+      threadControl.rawThread?.cancel()
+    }
+  }
 
-	@objc func threadFunc(_ object: Any?) {
-		let message = object as? String ?? ""
-		print("\(Thread.current.name ?? "") message=\(message)")
-	}
-	
+  @objc func threadFunc(_ object: Any?) {
+    let message = object as? String ?? ""
+    print("\(Thread.current.name ?? "") message=\(message)")
+  }
+  
 }
 
